c159c900889a900039ff5999326c6057281c553e
Suhaib Abu-Raidah <suhaib.aburaidah@gmail.com>
2025-06-11 03:47:24 +0300

    vis_results added to visualize segmentation masks

--- demo/vis_results.py
+++ demo/vis_results.py
@@ -6,15 +6,15 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--file_name", type=str)
 args, _ = parser.parse_known_args()
 
-
 data = np.load(f"./demo/results/{args.file_name}",allow_pickle=True).item()
 coord = data['xyz']
 rgb = data['rgb']-np.array([0.5,0.5,0.5])
 mask = data['mask']
-print(np.where(mask[1]==True)[0].shape)
-mask0 = mask[1].astype(bool)
+print(mask.shape)
+print(np.where(mask[0]==True)[0].shape)
+mask0 = mask[0].astype(bool)
 colors = np.zeros_like(coord)  # Shape: (N, 3)
-colors[mask0] = [1, 0, 0]      # Red for True
+colors[mask0] = [0, 0, 0]      # Red for True
 colors[~mask0] = [0, 0, 1]     # Blue for False
 points = np.hstack((coord,rgb))
 point_cloud = o3d.geometry.PointCloud()
