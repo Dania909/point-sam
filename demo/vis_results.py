998270c6c83bf25eedfec0d568d2cd8f7bcb00bd
Suhaib Abu-Raidah <suhaib.aburaidah@gmail.com>
2025-06-13 00:28:22 +0300

    Just visualization manipulation

--- demo/vis_results.py
+++ demo/vis_results.py
@@ -11,13 +11,19 @@ coord = data['xyz']
 rgb = data['rgb']-np.array([0.5,0.5,0.5])
 mask = data['mask']
 print(mask.shape)
-print(np.where(mask[0]==True)[0].shape)
+print(np.where(mask[1]==True)[0].shape)
 mask0 = mask[0].astype(bool)
+mask1 = mask[1].astype(bool)
+# mask2 = mask[2].astype(bool)
+# mask3 = mask[3].astype(bool)
 colors = np.zeros_like(coord)  # Shape: (N, 3)
-colors[mask0] = [0, 0, 0]      # Red for True
-colors[~mask0] = [0, 0, 1]     # Blue for False
+colors[mask0] = [1, 0, 0]      # Red for True
+colors[mask1] = [0, 0, 1]     # Blue for False
+# colors[mask2] = [1, 0.5, 0]      # Red for True
+# colors[mask3] = [1, 0, 1]     # Blue for False
 points = np.hstack((coord,rgb))
 point_cloud = o3d.geometry.PointCloud()
 point_cloud.points = o3d.utility.Vector3dVector(coord)
 point_cloud.colors = o3d.utility.Vector3dVector(colors)
 o3d.visualization.draw_geometries([point_cloud])
+
