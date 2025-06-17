16a946d64a0b298fb1711d5fa7a3d80bf88b4fb3
Suhaib Abu-Raidah <suhaib.aburaidah@gmail.com>
2025-06-18 00:15:01 +0300

    Some minor changes, nothing important

--- demo/vis_results.py
+++ demo/vis_results.py
@@ -14,12 +14,13 @@ print(mask.shape)
 print(np.where(mask[1]==True)[0].shape)
 mask0 = mask[0].astype(bool)
 mask1 = mask[1].astype(bool)
-# mask2 = mask[2].astype(bool)
+mask2 = mask[2].astype(bool)
 # mask3 = mask[3].astype(bool)
 colors = np.zeros_like(coord)  # Shape: (N, 3)
+colors[:]=[0.2,0.2,0.2]
 colors[mask0] = [1, 0, 0]      # Red for True
 colors[mask1] = [0, 0, 1]     # Blue for False
-# colors[mask2] = [1, 0.5, 0]      # Red for True
+colors[mask2] = [1, 0.5, 0]      # Red for True
 # colors[mask3] = [1, 0, 1]     # Blue for False
 points = np.hstack((coord,rgb))
 point_cloud = o3d.geometry.PointCloud()
